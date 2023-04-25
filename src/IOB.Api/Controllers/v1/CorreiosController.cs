b97afcb0ef467a9587bae0970a42a94eab3b8b32
Flavio Apolinario <flavioapolinario1@gmail.com>
2023-04-25 18:30:06 -0300

    Implemented Correios WebService  method ConsultaCEPAsync

---
+++ src/IOB.Api/Controllers/v1/CorreiosController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using IOB.Api.Controllers.Shared;
+using IOB.Application.DTO.Response;
+using IOB.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IOB.Api.Controllers.v1;
+
+[ApiVersion("1.0")]
+public class CorreiosController : ApiControllerBase
+{
+    private ICorreiosService _correiosService;
+    public readonly IMapper _mapper;
+
+    public CorreiosController(ICorreiosService correiosService, IMapper mapper)
+    {
+        _correiosService = correiosService;
+        _mapper = mapper;
+    }
+
+    [ProducesResponseType(typeof(IEnumerable<CompromissoResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    [HttpGet("{cep}")]
+    public async Task<ActionResult<EnderecoResponse>> ObterEnderecoPorCep(string cep)
+    {
+        var endereco = await _correiosService.ObterEnderecoPorCepAsync(cep);
+        if (endereco is null)
+            return NotFound();
+        
+        return Ok(endereco);
+    }
+}
