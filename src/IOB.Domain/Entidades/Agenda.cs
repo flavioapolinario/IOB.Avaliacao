8363154064d5a761c280de7afcc331e456ef60f6
Flavio Apolinario <flavioapolinario1@gmail.com>
2024-04-21 20:10:35 -0300

    Implementação da Agenda

---
+++ src/IOB.Domain/Entidades/Agenda.cs
@@ -0,0 +1,8 @@
+﻿namespace IOB.Domain.Entidades;
+
+public class Agenda
+{
+    public DateTime Inicio{ get; set; }
+    public DateTime Fim { get; set; }
+    public string? Descricao { get; set; }
+}
