9dd40a946ff807edb59fe3bea8047b7dc4b232b3
Flavio Apolinario <flavioapolinario1@gmail.com>
2024-04-21 20:15:53 -0300

    Revert "Implementação da Agenda"
    This reverts commit 8363154064d5a761c280de7afcc331e456ef60f6.

--- src/IOB.Domain/Entidades/Agenda.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IOB.Domain.Entidades;
-
-public class Agenda
-{
-    public DateTime Inicio{ get; set; }
-    public DateTime Fim { get; set; }
-    public string? Descricao { get; set; }
-}
